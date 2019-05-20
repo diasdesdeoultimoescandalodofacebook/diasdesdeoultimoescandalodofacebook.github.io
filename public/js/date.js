1ca3301bf8fba9b232d9e78f2c657c185e85b2f0
Reginildo Sousa <reginildosousa01@gmail.com>
2019-05-20 17:11:29 -0300

    Adicionando escandalo de vunerabilidade no whatsapp ratreamento de mensagem no whatsapp.

--- public/js/date.js
+++ public/js/date.js
@@ -1,6 +1,6 @@
 // The date parameter is actually the day of month, if absent then 1 is assumed.
 // Same for days.
-const lastScandalDate = new Date(2019, 3, 24)
-const currentDate = Date.now()
+const lastScandalDate = new Date(2019, 4, 12);
+const currentDate = Date.now();
 
-let outputDate = Math.floor((currentDate - lastScandalDate) / 24 / 3600 / 1000)
+let outputDate = Math.floor((currentDate - lastScandalDate) / 24 / 3600 / 1000);

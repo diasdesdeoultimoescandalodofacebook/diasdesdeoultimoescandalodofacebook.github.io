005104bcbcd5f20d59781025e7231220916e6514
Reginildo Sousa <reginildosousa01@gmail.com>
2019-10-11 16:09:37 -0300

    (21/05/2019) Facebook caught lying to the Dutch Parliament

--- public/js/date.js
+++ public/js/date.js
@@ -1,6 +1,6 @@
 // The date parameter is actually the day of month, if absent then 1 is assumed.
 // Same for days.
-const lastScandalDate = new Date(2019, 4, 12);
+const lastScandalDate = new Date(2019, 4, 21);
 const currentDate = Date.now();
 
 let outputDate = Math.floor((currentDate - lastScandalDate) / 24 / 3600 / 1000);

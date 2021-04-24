1e05a80d67c9d2da1923b02654c1a656b1a6f414
Reginildo Sousa <reginildosousa01@gmail.com>
2021-04-24 10:16:02 -0300

    Fix scandal.js

--- public/js/scandals.js
+++ public/js/scandals.js
@@ -1,6 +1,6 @@
 // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
 
-fetch("scandals/scandals.json")
+fetch("public/scandals/scandals.json")
   .then(
     function(response) {
       
